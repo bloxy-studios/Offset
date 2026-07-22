65533761ec992f6555ab90ca3d7d8816d1bdf6cf
bloxy-studios <abdulsdevworkspace@gmail.com>
2026-07-22 02:16:02 -0400

    Initial Commit

---
+++ Offset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Offset
+//
+//  Created by Abdul Karim Ali on 7/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Offset/OffsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffsetApp.swift
+//  Offset
+//
+//  Created by Abdul Karim Ali on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct OffsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
